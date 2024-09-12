32818ad5e271c289327f923b86c8c5019152f13f
ShahwaizQasim <syedshahwaiz32@gmail.com>
2024-09-12 11:43:47 +0500

    calling api get products

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,33 @@ import Card from './components/card'
 
 function App() {
   const [products, setProducts] = useState([])
-  console.log("products", products);
-  
+  // console.log("products", products);
+
 
   useEffect(() => {
-      fetchAllProducts();
+    fetchAllProducts();
   }, [])
 
-  const fetchAllProducts = async() => {
-       const products2 = await getAllProducts();
-       setProducts([...products2])
-       console.log(products2);
+  const fetchAllProducts = async () => {
+    const products2 = await getAllProducts();
+    setProducts([...products2])
+    //  console.log(products2);
   }
 
   return (
     <>
       <h2 className='text-3xl font-normal'>Shopping List</h2>
       <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4">
-      {/* {
-        products2.map((data)=> {
-          return <Card key={data.id} /> 
-        })
-      } */}
-    </div>
-  </div>
-</section>
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {
+              products.map((data) => {
+                return <Card key={data.id} item={data} />
+              })
+            }
+          </div>
+        </div>
+      </section>
 
     </>
   )
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,29 +1,28 @@
-function Card({item}) {
+function Card({ item }) {
 
-    const [categories, ] = item;
+  const {category, description, images, price, title} = item
 
   return (
     <div className="p-4 md:w-1/3">
       <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
         <img
           className="lg:h-48 md:h-36 w-full object-cover object-center"
-          src="https://dummyimage.com/720x400"
+          src={`${images[0]}`}
           alt="blog"
         />
         <div className="p-6">
           <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-            CATEGORY
+            {category.name}
           </h2>
           <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-            The Catalyzer
+            {title}
           </h1>
           <p className="leading-relaxed mb-3">
-            Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-            microdosing tousled waistcoat.
+            {description}
           </p>
           <div className="flex items-center flex-wrap ">
             <a className="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">
-              Learn More
+              Add to Cart
               <svg
                 className="w-4 h-4 ml-2"
                 viewBox="0 0 24 24"
@@ -37,7 +36,7 @@ function Card({item}) {
                 <path d="M12 5l7 7-7 7" />
               </svg>
             </a>
-            <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+            {/* <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
               <svg
                 className="w-4 h-4 mr-1"
                 stroke="currentColor"
@@ -51,8 +50,8 @@ function Card({item}) {
                 <circle cx={12} cy={12} r={3} />
               </svg>
               1.2K
-            </span>
-            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+            </span> */}
+            {/* <span className="text-gray-400 inline-flex items-center leading-none text-sm">
               <svg
                 className="w-4 h-4 mr-1"
                 stroke="currentColor"
@@ -65,7 +64,7 @@ function Card({item}) {
                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
               </svg>
               6
-            </span>
+            </span> */}
           </div>
         </div>
       </div>
