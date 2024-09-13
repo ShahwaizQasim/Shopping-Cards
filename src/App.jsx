0624eba844b397ec7d62f721d0a19e8c3780e0b2
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-09-12 22:25:06 -0700

    cards show

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,36 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import getAllProducts from './utils/shoppingCards'
-import Card from './components/card'
+import { useState, useEffect } from "react";
+import "./App.css";
+import getAllProducts from "./utils/shoppingCards";
+import Card from "./components/card";
 
 function App() {
-  const [products, setProducts] = useState([])
-  // console.log("products", products);
-
+  const [products, setProducts] = useState([]);
+  console.log("products", products);
 
   useEffect(() => {
     fetchAllProducts();
-  }, [])
+  }, []);
 
   const fetchAllProducts = async () => {
     const products2 = await getAllProducts();
-    setProducts([...products2])
-    //  console.log(products2);
-  }
+    setProducts([...products2]);
+     console.log(products2);
+  };
 
   return (
     <>
-      <h2 className='text-3xl font-normal'>Shopping List</h2>
+    <h2 className=" text-center text-3xl font-normal">Shopping List</h2>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            {
-              products.map((data) => {
-                return <Card key={data.id} item={data} />
-              })
-            }
+            {products.map((data) => {
+              return <Card key={data.id} item={data} />;
+            })}
           </div>
         </div>
       </section>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/utils/shoppingCards.js
+++ src/utils/shoppingCards.js
@@ -3,7 +3,8 @@
 async function getAllProducts(){
     try {
         const CARDS_API = await fetch('https://api.escuelajs.co/api/v1/products');
-        const products = CARDS_API.json()
+        const products = await CARDS_API.json()
+        console.log("products", products);
         return products;
     } catch (error) {
         console.log(error.message);
